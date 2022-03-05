95ae2f400c1233cd2f2db8c8222b53cd76147825
samin lashgari <saminlashgari0@gmail.com>
2022-03-05 11:48:24 +0330

    dictionary methods

---
+++ dict.py
@@ -0,0 +1,7 @@
+dict = {"samin" : "thamin" , "sana" : "holy cow" , "sogand" : "sogand*"}
+print(dict.get("sara" , "saboon"))
+print(dict)
+print(dict.pop("sogand"))
+print(dict)
+print(dict.update({"hanie" : "hon"}))
+print(dict)
